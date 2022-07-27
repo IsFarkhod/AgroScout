4fdf695efae4bfe1fa1fbbfb733ed40b1c962aaf
Farkhod17 <sempaifsopz30@gmail.com>
2022-07-27 16:57:02 +0500

    add style for sections

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,10 +2,10 @@ const arrow = document.querySelector('.about_company__img')
 const show = document.querySelector('.about_company__show')
 
 
-arrow.addEventListener('click', () => {
+console.log(arrow.addEventListener('click', function() {
     if (show.classList.toggle("about_company--active")) {
         arrow.classList.add("about_company--rotate")
     } else {
         arrow.classList.remove("about_company--rotate")
     }
-})
+}))
