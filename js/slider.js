061c5cd8c62706e5f86949e72d738936cbcf7a4e
Farkhod17 <sempaifsopz30@gmail.com>
2022-10-18 21:30:36 +0300

    start catalog page

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+const sliderCard1 = document.querySelector(".slider__card-1")
+const sliderCard2 = document.querySelector(".slider__card-2")
+const sliderCard3 = document.querySelector(".slider__card-3")
+
+setInterval( () => {
+    sliderCard1.classList.add(".anim")
+}, 3000)
